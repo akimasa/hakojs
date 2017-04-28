2156cf0835fc6083d819148991c91e73467bca07
Akimasa Tateba <akimasa@gmail.com>
2017-04-28 15:21:52 +0900

    tests

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,9 @@
 import * as turn from "./hako-turn";
 import * as lib from "./lib";
-let gIslands = [
-    {
-        name: "hogesima",
-        id: 0,
-        prize: "hogehoge", // TBD
-        absent: 2,
-        comment: "yahoo",
-        password: "crypted",
-        money: 1,
-        food: 1,
-        pop: 1,
-        area: 1,
-        farm: 1,
-        factory: 1,
-        mountain: 1,
-        score: 1,
-        lands: [[{ land: 0, value: 0 }, { land: 0, value: 0 }, { land: 0, value: 0 }],
-        [{ land: 0, value: 0 }, { land: 0, value: 0 }, { land: 0, value: 0 }],
-        [{ land: 0, value: 0 }, { land: 0, value: 0 }, { land: 0, value: 0 }]],
-        commands: [{ kind: 0, target: 0, x: 0, y: 0, arg: 0 }],
-        bbs: [""],
-    },
-];
+let gIslands = require("../testfile.json");
 let gNextId = 1;
 let ret = turn.newIslandMain({
-    name: "hoge",
+    name: "hige",
     password: "pass",
     password2: "pass",
     islands: gIslands,
@@ -39,7 +17,7 @@ if (ret.err) {
 }
 for (let i = 0; i < 10; i++) {
     ret = turn.newIslandMain({
-        name: "hoge" + i,
+        name: "hige" + i,
         password: "pass",
         password2: "pass",
         islands: gIslands,
