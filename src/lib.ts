174f08604df19949895569920e4467753878ba49
Akimasa Tateba <akimasa@gmail.com>
2017-04-28 22:03:25 +0900

    settings

--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,5 @@
 export { lands as lands } from "./lands";
-export { default as hako } from "./settings";
+export { default as settings } from "./settings";
 export { default as Hakojima } from "./Hakojima";
 export { default as Island } from "./Island";
 export { Land as Land } from "./Island";
