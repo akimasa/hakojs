4b96a26114f17e316196239b5fa359795a17f5e6
Akimasa Tateba <akimasa@gmail.com>
2017-04-30 23:07:29 +0900

    haribote web index

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,9 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use("/", express.static(`${__dirname}/../static/`));
+app.use("/js/", express.static(`${__dirname}/../release/web/`));
+
 app.get("/api/island/:id", (req, res, next) => {
     req.params.id = parseInt(req.params.id, 10);
     const island = hako.getIsland(req.params.id);
