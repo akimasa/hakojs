a98c6adbb4d7845c0505a43c69b25db672c32a60
Akimasa Tateba <akimasa@gmail.com>
2017-05-03 20:47:34 +0900

    IslandMap.ts

--- web/IslandMap/IslandMap.ts
+++ web/IslandMap/IslandMap.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 
+import { lands as Lands } from "../../src/lands";
 import * as Template from "./IslandMap.html";
 
 @Template
@@ -29,38 +30,40 @@ export default class IslandMap extends Vue {
         private landstr(data) {
         let image = "";
         let alt = "";
-        if (data.kind === 0) {
-            if (data.value === 1) {
+        const kind = data.kind;
+        const value = data.value;
+        if (kind === Lands.Sea) {
+            if (value === 1) {
                 image = "land14.gif";
                 alt = "海（浅瀬）";
             } else {
                 image = "land0.gif";
             }
-        } else if (data.kind === 1) {
-            if (data.value === 1) {
+        } else if (kind === Lands.Waste) {
+            if (value === 1) {
                 image = "land13.gif";
             } else {
                 image = "land1.gif";
             }
-        } else if (data.kind === 2) {
+        } else if (kind === Lands.Plains) {
             image = "land2.gif";
-        } else if (data.kind === 4) {
+        } else if (kind === Lands.Forest) {
             image = "land6.gif";
-        } else if (data.kind === 3) {
-            if (data.value < 3) {
+        } else if (kind === Lands.Town) {
+            if (value < 3) {
                 image = "land3.gif";
-            } else if (data.value < 100) {
+            } else if (value < 100) {
                 image = "land4.gif";
             } else {
                 image = "land5.gif";
             }
 
-        } else if (data.kind === 7) {
+        } else if (kind === Lands.Base) {
             image = "land9.gif";
-        } else if (data.kind === 9) {
+        } else if (kind === Lands.Mountain) {
             image = "land11.gif";
         } else {
-            alt = data.kind;
+            alt = kind;
         }
         return "img/" + image;
     }
