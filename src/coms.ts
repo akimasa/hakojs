8a2d071edd7b1fbc73cedcf0d8e2ff25df7d3ad8
Akimasa Tateba <akimasa@gmail.com>
2017-05-27 18:35:05 +0900

    fix unnecessarily quoted property

--- src/coms.ts
+++ src/coms.ts
@@ -1,126 +1,126 @@
 const coms = {
     coms: {
-        "prepare": {
+        prepare: {
             name: "整地",
             cost: 5,
             id: 1,
         },
-        "prepare2": {
+        prepare2: {
             name: "地ならし",
             cost: 100,
             id: 2,
         },
-        "reclaim": {
+        reclaim: {
             name: "埋め立て",
             cost: 150,
             id: 3,
         },
-        "destroy": {
+        destroy: {
             name: "掘削",
             cost: 200,
             id: 4,
         },
-        "selltree": {
+        selltree: {
             name: "伐採",
             cost: 0,
             id: 5,
         },
-        "plant": {
+        plant: {
             name: "植林",
             cost: 50,
             id: 11,
         },
-        "farm": {
+        farm: {
             name: "農場整備",
             cost: 20,
             id: 12,
         },
-        "factory": {
+        factory: {
             name: "工場建設",
             cost: 100,
             id: 13,
         },
-        "mountain": {
+        mountain: {
             name: "採掘場整備",
             cost: 300,
             id: 14,
         },
-        "base": {
+        base: {
             name: "ミサイル基地建設",
             cost: 300,
             id: 15,
         },
-        "dbase": {
+        dbase: {
             name: "防衛施設建設",
             cost: 800,
             id: 16,
         },
-        "sbase": {
+        sbase: {
             name: "海底基地建設",
             cost: 8000,
             id: 17,
         },
-        "monument": {
+        monument: {
             name: "記念碑建造",
             cost: 9999,
             id: 18,
         },
-        "haribote": {
+        haribote: {
             name: "ハリボテ設置",
             cost: 1,
             id: 19,
         },
-        "missileNM": {
+        missileNM: {
             name: "ミサイル発射",
             cost: 20,
             id: 31,
         },
-        "missilePP": {
+        missilePP: {
             name: "PPミサイル発射",
             cost: 50,
             id: 32,
         },
-        "missileST": {
+        missileST: {
             name: "STミサイル発射",
             cost: 50,
             id: 33,
         },
-        "missileLD": {
+        missileLD: {
             name: "陸地破壊弾発射",
             cost: 100,
             id: 34,
         },
-        "sendmonster": {
+        sendmonster: {
             name: "怪獣派遣",
             cost: 3000,
             id: 35,
         },
-        "donothing": {
+        donothing: {
             name: "資金繰り",
             cost: 0,
             id: 41,
         },
-        "sell": {
+        sell: {
             name: "食料輸出",
             cost: -100,
             id: 42,
         },
-        "money": {
+        money: {
             name: "資金援助",
             cost: 100,
             id: 43,
         },
-        "food": {
+        food: {
             name: "食糧援助",
             cost: -100,
             id: 44,
         },
-        "propaganda": {
+        propaganda: {
             name: "誘致活動",
             cost: 1000,
             id: 45,
         },
-        "giveup": {
+        giveup: {
             name: "島の放棄",
             cost: 0,
             id: 46,
